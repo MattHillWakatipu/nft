935ada9671bbbdd1c5befa6896d449377c041d52
phillipchung <mrphillipchung@gmail.com>
2021-12-03 15:54:02 +1300

    the working one2

--- nft/src/lib.rs
+++ nft/src/lib.rs
@@ -15,6 +15,7 @@ NOTES:
   - To prevent the deployed contract from being modified or deleted, it should not have any access
     keys on its account.
 */
+use std::borrow::Borrow;
 use std::collections::HashMap;
 use near_contract_standards::non_fungible_token::core::NonFungibleTokenCore;
 use near_contract_standards::non_fungible_token::core::NonFungibleTokenResolver;
@@ -36,6 +37,7 @@ near_sdk::setup_alloc!();
 pub struct Contract {
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
+    owner: ValidAccountId,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -76,12 +78,13 @@ impl Contract {
         Self {
             tokens: NonFungibleToken::new(
                 StorageKey::NonFungibleToken,
-                owner_id,
+                owner_id.clone(),
                 Some(StorageKey::TokenMetadata),
                 Some(StorageKey::Enumeration),
                 Some(StorageKey::Approval),
             ),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+            owner: owner_id,
         }
     }
 
@@ -188,9 +191,11 @@ impl NonFungibleTokenCore for Contract {
 
         let new_id = token_id.to_string().add(";)");
         let sender_id = env::predecessor_account_id();
+        let thing : ValidAccountId = self.owner.clone();
         // let mint_id = ValidAccountId(memo.unwrap());
         self.tokens.internal_transfer(&sender_id, receiver_id.as_ref(), &token_id, None, Some("thevarusspreads".into()));
-        self.nft_mint(new_id.to_string(), receiver_id, metadata);
+        self.nft_mint(new_id.to_string(), thing , metadata);
+        self.tokens.internal_transfer(&sender_id, receiver_id.as_ref(), new_id.to_string().borrow(), None, Some("thevarusspreadsAgain".into()));
     }
 
 
